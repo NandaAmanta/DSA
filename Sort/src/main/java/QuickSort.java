59dfd17420578d0a6fb11cd628db08b0fb1fdcb9
Nanda Amanta <nansamgr@gmail.com>
2022-07-16 21:57:10 +0800

    init quicksort

---
+++ Sort/src/main/java/QuickSort.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class QuickSort implements Sort{
+
+    @Override
+    public void sort(int[] array) {
+        
+        
+    }
+
+    private void sort(int[] array , int lowIndex, int highIndex){
+        
+    }
+    
+    @Override
+    public void sort(String[] array) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void sort(char[] array) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void sort(double[] array) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
